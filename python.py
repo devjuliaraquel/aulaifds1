24df1deebba5bdccc41cc0dd62c0c93ba3abde2f
devjuliaraquel <julia.celes@aluno.ifsp.edu.br>
2024-08-07 11:45:21 -0300

    Add files via upload

---
+++ python.py
@@ -0,0 +1,6 @@
+n1 = float(input("Digite a nota obtida no 1º: "))
+n2 = float(input("Digite a nota obtida no 2º: "))
+
+media = (n1+n2)/2
+
+print("Sua media final é:",  media)
---
+++ script.js
@@ -0,0 +1,7 @@
+// criando uma variavel
+let mensagem1= "ola, mundo!"
+let mensagem2= "ola, ola"
+
+// exibir resultado
+console.log(mensagem1);
+console.log(mensagem2);
